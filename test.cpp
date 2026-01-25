8105044664f1a0873970543cbd2624ca62a25251
SteadyOsi <Josiahstedman@gmail.com>
2026-01-26 10:43:48 +1100

    Messing with C++

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int numbers[10] = {1,22,3,4,35,6,75,8,9,10};
+
+    for (int i = 0; i < 10; i++){
+        std::cout << numbers[i] << std::endl; 
+    }
+
+    return 0;
+}
+
+
