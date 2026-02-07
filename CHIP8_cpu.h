e98e6908425638a46e54a0c15a94d33cf112de0f
SteadyOsi <Josiahstedman@gmail.com>
2026-02-07 17:40:35 +1100

    filling in opcode constructors

--- CHIP8_cpu.h
+++ CHIP8_cpu.h
@@ -36,30 +36,69 @@ private:
     void increment();
 
     // 0nnn - SYS addr (ignored / legacy)
+
     // 00E0 - CLS
+    void execute_CLS();
+
     // 00EE - RET
+    void execute_RET();
 
     // 1nnn - JP addr
+    void execute_JP(uint16_t nnn);
+
     // 2nnn - CALL addr
+    void execute_CALL(uint16_t nnn);
+
     // 3xnn - SE Vx, byte
+    void execute_SE_vx_kk(uint8_t vx, uint8_t kk);
+
     // 4xnn - SNE Vx, byte
+    void execute_SNE_vx_kk(uint8_t vx, uint8_t kk);
+
     // 5xy0 - SE Vx, Vy
+    void execute_SE_vx_vy(uint8_t vx, uint8_t vy);
+
     // 6xnn - LD Vx, byte
+    void execute_LD_vx_kk(uint8_t vx, uint8_t kk)
+
     // 7xnn - ADD Vx, byte
+    void execute_ADD_vx_kk(uint8_t vx, uint8_t kk);
 
     // 8xy0 - LD Vx, Vy
+    void execute_LD_vx_vy(uint8_t vx, uint8_t vy);
+
     // 8xy1 - OR Vx, Vy
+    void execute_OR_vx_vy(uint8_t vx, uint8_t vy);
+
     // 8xy2 - AND Vx, Vy
+    void execute_AND_vx_vy(uint8_t vx, uint8_t vy);
+
     // 8xy3 - XOR Vx, Vy
+    void execute_XOR_vx_vy(uint8_t vx, uint8_t vy);
+
     // 8xy4 - ADD Vx, Vy
+    void execute_ADD_vx_vy(uint8_t vx, uint8_t vy);
+
     // 8xy5 - SUB Vx, Vy
+    void execute_SUB_vx_vy(uint8_t vx, uint8_t vy);
+
     // 8xy6 - SHR Vx
+    void execute_SHR_vx(uint8_t vx);
+
     // 8xy7 - SUBN Vx, Vy
+    void execute_SUBN_vx_vy(uint8_t vx, uint8_t vy);
+
     // 8xyE - SHL Vx
+    void execute_SHL_vx(uint8_t vx);
 
     // 9xy0 - SNE Vx, Vy
+    void execute_SNE_vx_vy(uint8_t vx, uint8_t vy);
+
     // Annn - LD I, addr
+    void execute_LD_i_nnn(uint16_t nnn);
+
     // Bnnn - JP V0, addr
+
     // Cxnn - RND Vx, byte
     // Dxyn - DRW Vx, Vy, nibble
 
